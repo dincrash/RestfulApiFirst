3c2535b4ddab8edba6e482a33e896b5e385068d7
Vladislav <dincrash@yandex.ru>
2018-11-25 21:03:36 +0300

    first comit, add hello world)

---
+++ src/main/java/com/example/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.hello")
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
